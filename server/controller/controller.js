d99b5c003bcc323c760d52fbcda51d24ccd498c8
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-10-04 19:57:54 +0300

    created CRUD operations and functions on backend

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -15,13 +15,74 @@ exports.create = (req, res) => {
     gender: req.body.gender,
     status: req.body.status,
   });
+
+  // save user
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) =>
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating a save operation",
+      })
+    );
 };
 
 // retrieve and return all users / retrieve and return a single user
-exports.find = (req, res) => {};
+exports.find = (req, res) => {
+  Userdb.find()
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Error while selecting existing user / Usersb.find",
+      });
+    });
+};
 
 // update a new identified user by user id
-exports.update = (req, res) => {};
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({ message: "Data cannot be empty" });
+  }
+
+  const id = req.params.id;
+  Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(400).send({
+          message: `Cannot update user with ID: ${id}. User not found!`,
+        });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error, update user information" });
+    });
+};
 
 // delete a user with specified user id
-exports.delete = (req, res) => {};
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Userdb.findByIdAndDelete(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete user with ID: ${id}. User not found.`,
+        });
+      } else {
+        res.send({
+          message: "User was deleted successfully!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Could not delete user." });
+    });
+};
