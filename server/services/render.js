a2118c94853a1c88dede90acf74a0f88a5675951
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-10-10 02:35:41 +0300

    rendered data from the db

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,15 @@
+const axios = require("axios");
+
 exports.homeRoutes = (req, res) => {
-  res.render("index");
+  axios
+    .get("http://localhost:8080/api/users")
+    .then((response) => {
+      console.log(response.data);
+      res.render("index", { users: response.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 };
 
 exports.add_user = (req, res) => {
